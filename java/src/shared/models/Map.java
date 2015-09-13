74ad129f6bc951bc7787d9285522aa6eb8ad1421
zjensen <zsjensen@gmail.com>
2015-09-13 12:46:15 -0600

    Model creation
    setting up model classes

---
+++ java/src/shared/models/Map.java
@@ -0,0 +1,125 @@
+package shared.models;
+
+import shared.locations.HexLocation;
+
+public class Map {
+	private Hex[] hexes;
+	private Port[] ports;
+	private Road[] roads;
+	private VertexObject[] settlements;
+	private VertexObject[] cities;
+	private int radius;
+	private HexLocation robber;
+	
+	public Map(Hex[] hexes, Port[] ports, Road[] roads,
+			VertexObject[] settlements, VertexObject[] cities, int radius,
+			HexLocation robber) 
+	{
+		super();
+		this.hexes = hexes;
+		this.ports = ports;
+		this.roads = roads;
+		this.settlements = settlements;
+		this.cities = cities;
+		this.radius = radius;
+		this.robber = robber;
+	}
+
+	/**
+	 * @return the hexes
+	 */
+	public Hex[] getHexes() {
+		return hexes;
+	}
+
+	/**
+	 * @param hexes the hexes to set
+	 */
+	public void setHexes(Hex[] hexes) {
+		this.hexes = hexes;
+	}
+
+	/**
+	 * @return the ports
+	 */
+	public Port[] getPorts() {
+		return ports;
+	}
+
+	/**
+	 * @param ports the ports to set
+	 */
+	public void setPorts(Port[] ports) {
+		this.ports = ports;
+	}
+
+	/**
+	 * @return the roads
+	 */
+	public Road[] getRoads() {
+		return roads;
+	}
+
+	/**
+	 * @param roads the roads to set
+	 */
+	public void setRoads(Road[] roads) {
+		this.roads = roads;
+	}
+
+	/**
+	 * @return the settlements
+	 */
+	public VertexObject[] getSettlements() {
+		return settlements;
+	}
+
+	/**
+	 * @param settlements the settlements to set
+	 */
+	public void setSettlements(VertexObject[] settlements) {
+		this.settlements = settlements;
+	}
+
+	/**
+	 * @return the cities
+	 */
+	public VertexObject[] getCities() {
+		return cities;
+	}
+
+	/**
+	 * @param cities the cities to set
+	 */
+	public void setCities(VertexObject[] cities) {
+		this.cities = cities;
+	}
+
+	/**
+	 * @return the radius
+	 */
+	public int getRadius() {
+		return radius;
+	}
+
+	/**
+	 * @param radius the radius to set
+	 */
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
+	/**
+	 * @return the robber
+	 */
+	public HexLocation getRobber() {
+		return robber;
+	}
+
+	/**
+	 * @param robber the robber to set
+	 */
+	public void setRobber(HexLocation robber) {
+		this.robber = robber;
+	}
+}
