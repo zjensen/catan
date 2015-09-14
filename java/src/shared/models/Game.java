e4ede4ddf6784511eabfa0429c633c6e2c45c719
zjensen <zsjensen@gmail.com>
2015-09-14 16:36:41 -0600

    Adding models and update .gitignore

---
+++ java/src/shared/models/Game.java
@@ -0,0 +1,96 @@
+package shared.models;
+
+/**
+ * FOR LOGIN PAGE. Displays all games going on
+ * @author zsjensen
+ *
+ */
+public class Game {
+	private ClientModel clientModel;
+	private String title;
+	private int id;
+	
+	/**
+	 * check if there is space in game
+	 * @return true if user can join game, else false
+	 */
+	public boolean canJoin()
+	{
+		return false;
+	}
+	
+	/**
+	 * check if user is in this game
+	 * @return true if user is in this game, else false
+	 */
+	public boolean inGame()
+	{
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @return string of each player name in game
+	 */
+	public String listPlayers()
+	{
+		StringBuilder sb = new StringBuilder();
+		Player[] players = clientModel.getPlayers();
+		for(Player p : players)
+		{
+			sb.append(player.getName());
+			sb.append(", ");
+		}
+		
+		if(sb.length() > 0)
+		{
+			sb.setLength(sb.length() - 2);
+		}
+		return sb.toString();
+	}
+
+	/**
+	 * @return the clientModel
+	 */
+	public ClientModel getClientModel() {
+		return clientModel;
+	}
+
+	/**
+	 * @param clientModel the clientModel to set
+	 */
+	public void setClientModel(ClientModel clientModel) {
+		this.clientModel = clientModel;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	//is in game(id)
+	
+}
