96906c537a082b583e15026cba3d63d07f4ee9ea
Trevor Martin <iceman1780@gmail.com>
2015-09-21 00:01:22 -0600

    UserFacade stuff
    I hope this works

---
+++ java/src/server/facade/user/UserFacade.java
@@ -0,0 +1,33 @@
+package server.facade.user;
+
+import shared.communication.user.LoginResult;
+import shared.communication.user.NewUserInfo;
+import shared.communication.user.RegisterResult;
+import shared.communication.user.UserCredentials;
+
+
+
+
+public class UserFacade {
+
+	
+	
+	public LoginResult userLogin (UserCredentials params) {
+//		user/login
+//	    VALID INPUT
+//	        username: SAM
+//	        password: sam
+		return null;
+	}
+	
+	public RegisterResult newUser (NewUserInfo params) {
+//    user/register
+//    VALID INPUT -- but only the first time you try it with the name 
+//        username: tiger
+//        password: shark
+		return null;
+	}
+	
+	
+	
+}
---
+++ java/src/shared/communication/user/LoginResult.java
@@ -0,0 +1,5 @@
+package shared.communication.user;
+
+public class LoginResult {
+
+}
