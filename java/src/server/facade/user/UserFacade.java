084fd24aa9e7cb74dbcc7ede6de0382992d2302e
Trevor Martin <iceman1780@gmail.com>
2015-09-21 00:51:17 -0600

    UserFacade javadoc

--- java/src/server/facade/user/UserFacade.java
+++ java/src/server/facade/user/UserFacade.java
@@ -4,11 +4,24 @@ import shared.communication.user.*;
 
 public class UserFacade {
 
+	
+	/**
+	 * Logs into the server with the provided credentials
+	 * 
+	 * @param params
+	 * @return Login_Output
+	 */
 	public Login_Output login(Login_Input params){
 		
 		return null;
 	}
 	
+	/**
+	 * Registers a user with the provided credentials
+	 * 
+	 * @param params
+	 * @return Register_Output
+	 */
 	public Register_Output register(Register_Input params){
 		
 		return null;
