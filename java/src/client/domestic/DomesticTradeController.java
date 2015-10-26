7eb570609ed50de68b9ef8555ec8755e3c865685
zjensen <zsjensen@gmail.com>
2015-10-25 18:05:33 -0600

    Trade accept fix

--- java/src/client/domestic/DomesticTradeController.java
+++ java/src/client/domestic/DomesticTradeController.java
@@ -183,6 +183,10 @@ public class DomesticTradeController extends Controller implements IDomesticTrad
 				{
 					getAcceptOverlay().setAcceptEnabled(true);
 				}
+				else
+				{
+					getAcceptOverlay().setAcceptEnabled(false);
+				}
 			}
 			else
 			{
