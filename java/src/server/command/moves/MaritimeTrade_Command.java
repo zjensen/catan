3c49b74bf3a381737d74ba59d28c34e3d6c431f5
Davis Hyer <davishyer@gmail.com>
2015-11-11 17:26:37 -0700

    Maritime command obj

--- java/src/server/command/moves/MaritimeTrade_Command.java
+++ java/src/server/command/moves/MaritimeTrade_Command.java
@@ -25,15 +25,17 @@ public class MaritimeTrade_Command extends ServerCommand {
 	}
 
 	@Override
-	public JsonElement execute()
+	public JsonElement execute() throws ServerInvalidRequestException
 	{
-		return ServerManager.instance().getMovesFacade().maritimeTrade(params, super.playerId, super.gameId);
+		return execute(super.json);
 	}
 
 	@Override
 	public JsonElement execute(String json) throws ServerInvalidRequestException {
-		// TODO Auto-generated method stub
-		return null;
+		// TODO get params from json
+		params = gson.fromJson(json, MaritimeTrade_Input.class);
+		
+		return ServerManager.instance().getMovesFacade().maritimeTrade(params, super.playerId, super.gameId);
 	}
 
 }
